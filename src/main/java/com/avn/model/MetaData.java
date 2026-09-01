82be13f65e2e7080a06cf97dc1a512a4c6867e51
c4182052@gmail.com <your-email@gmail.com>
2026-09-02 00:23:19 +0530

    Fix database connection, add MySQL driver, and correct timestamp annotations

--- src/main/java/com/avn/model/MetaData.java
+++ src/main/java/com/avn/model/MetaData.java
@@ -1,43 +1,30 @@
 package com.avn.model;
 
-import java.math.BigDecimal;
 import java.time.Instant;
 
-import org.hibernate.annotations.ColumnDefault;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Embeddable;
-import jakarta.persistence.Embedded;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
 import lombok.Getter;
 import lombok.Setter;
+
 @Embeddable
 @Getter
 @Setter
 public class MetaData {
-	//@Id
-//	@GeneratedValue(strategy=GenerationType.IDENTITY)
-//	private Long product_id;
-//	@Column(nullable = false)
-//	private String name;
-//	@Column(nullable = false)
-//	private String description;
-//	@Column(nullable = false)
-//	private BigDecimal price;
-//	@Column(nullable = false)
-//	private Integer popularity;
-//	private String imageUrl;
-	@ColumnDefault("CURRENT_TIMESTAMP")
+
+	@CreationTimestamp
+	@Column(updatable = false)
 	private Instant createdAt;
+	
 	@Column(nullable = false)
 	private String createdBy;
-	@ColumnDefault("null")
+	
+	@UpdateTimestamp
 	private Instant updatedAt;
-	@ColumnDefault("null")
+	
 	private String updatedBy;
-	
-	
 
 }
